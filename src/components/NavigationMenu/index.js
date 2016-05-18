813041cf3cfc452d81fead4e9029695e6975e3ea
RussHR <russ.rinzler@gmail.com>
2016-05-18 00:38:24 -0700

    add links

--- src/components/NavigationMenu/index.js
+++ src/components/NavigationMenu/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 import './navigation-menu.scss';
 
@@ -21,6 +22,32 @@ export default class NavigationMenu extends Component {
                     <div className="hamburger-line" />
                     <div className="hamburger-line" />
                 </div>
+                <ul className="navigation-links">
+                    <li>
+                        <Link to="/">about</Link>
+                    </li>
+                    <li>
+                        work
+                    </li>
+                    <li>
+                        <Link to="/work/credit-karma">credit karma</Link>
+                    </li>
+                    <li>
+                        <Link to="/work/granicus">granicus</Link>
+                    </li>
+                    <li>
+                        play
+                    </li>
+                    <li>
+                        <Link to="/play/orthofloat">orthofloat</Link>
+                    </li>
+                    <li>
+                        <Link to="/play/trendtrend">trendtrend</Link>
+                    </li>
+                    <li>
+                        <Link to="/links-contact">links & contact</Link>
+                    </li>
+                </ul>
             </div>
         );
     }
