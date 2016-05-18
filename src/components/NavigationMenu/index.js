057625778275a554ddf920aabb7169f51833cd94
RussHR <russ.rinzler@gmail.com>
2016-05-17 23:09:42 -0700

    has a menu that fires a toggle function

--- src/components/NavigationMenu/index.js
+++ src/components/NavigationMenu/index.js
@@ -3,10 +3,24 @@ import React, { Component } from 'react';
 import './navigation-menu.scss';
 
 export default class NavigationMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isOpen: false };
+    }
+
+    toggleMenu() {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
     render() {
         return (
             <div className="navigation-menu">
-                I am the home page
+                <h1 className="navigation-my-name">russ rinzler</h1>
+                <div className="navigation-menu-toggle" onClick={() => this.toggleMenu()}>
+                    <div className="hamburger-line" />
+                    <div className="hamburger-line" />
+                    <div className="hamburger-line" />
+                </div>
             </div>
         );
     }
