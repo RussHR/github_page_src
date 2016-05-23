8286d5e608b32877a770164b3954ef944edb4baa
RussHR <russ.rinzler@gmail.com>
2016-05-22 23:59:53 -0700

    add some safety to unmatched routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory, Redirect } from 'react-router';
 import MainLayout from './components/MainLayout';
 import Home from './components/Home';
 
@@ -9,6 +9,8 @@ export default class App extends Component {
             <Router history={hashHistory}>
                 <Route path="/" component={MainLayout}>
                     <IndexRoute component={Home}/>
+                    <Redirect from="about" to="/" />
+                    <Route path="*" component={Home} />
                 </Route>
             </Router>
         );
