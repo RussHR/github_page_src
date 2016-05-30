04292742b97b2fba8a7b2d63d30fbe1f856e95e5
RussHR <russ.rinzler@gmail.com>
2016-05-30 03:59:34 -0700

    add vimeo for orthofloat

--- src/components/OrthofloatPage/index.js
+++ src/components/OrthofloatPage/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import ContentLayout from '../ContentLayout';
 
+import './orthofloat-page.scss';
+
 export default function OrthofloatPage() {
     return (
         <ContentLayout header="trendtrend"
@@ -10,6 +12,14 @@ export default function OrthofloatPage() {
             <p>
                 wow three.js wow real description coming later!
             </p>
+            <div className="vimeo-wrapper">
+                <iframe src="https://player.vimeo.com/video/168572687"
+                    className="orthofloat-video"
+                    frameBorder="0"
+                    webkitAllowFullScreen
+                    mozallowfullscreen
+                    allowFullScreen />
+            </div>
         </ContentLayout>
     );
 }
