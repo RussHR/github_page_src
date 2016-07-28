3e65d6cf7fd51ae0362c4d25dedb6fc229bbda10
RussHR <russ.rinzler@gmail.com>
2016-07-27 23:00:29 -0700

    update the orthofloat page

--- src/components/pages/OrthofloatPage/index.js
+++ src/components/pages/OrthofloatPage/index.js
@@ -6,14 +6,14 @@ import VideoIFrame from '../../VideoIFrame';
 export default function OrthofloatPage() {
     return (
         <ContentLayout header="orthofloat"
-                       subheader="in progress"
+                       subheader="resolved - july 2016"
                        links={[ 'http://russrinzler.com/orthofloat', 'https://github.com/RussHR/orthofloat_src' ]}>
             <p>
                 orthofloat marks a foray into WebGL with three.js.
-                currently, you can actively make the cubes a random color.
+                you can change the colors and rotate the camera.
             </p>
-            <VideoIFrame style={{ width:"100%", paddingBottom: `${(417/640) * 100}%` }}
-                        src="https://player.vimeo.com/video/168572687" />
+            <VideoIFrame style={{ width:"100%", paddingBottom: `${(670/1226) * 100}%` }}
+                        src="https://player.vimeo.com/video/176560218" />
         </ContentLayout>
     );
 }
