c5e6fa350e19eb8f22bca1b0b1fea9148a64c908
Russ Rinzler <russ.rinzler@gmail.com>
2018-04-10 22:36:27 -0700

    add a package-lock.json and add gradient to background

--- src/components/layout/MainLayout/index.js
+++ src/components/layout/MainLayout/index.js
@@ -5,6 +5,7 @@ import './main-layout.scss';
 export default function MainLayout({ children }) {
     return (
         <div className="main-layout">
+            <div className="main-layout-bg" />
             <NavigationMenu />
             <div className="main-content">
                 {children}
