12ba380a6cf18bd026f3c34c65c623c59cb0496d
Russ Rinzler <russ.rinzler@gmail.com>
2018-04-11 22:28:58 -0700

    creates random triangle bg on start

--- src/components/layout/MainLayout/index.js
+++ src/components/layout/MainLayout/index.js
@@ -1,11 +1,22 @@
 import React, { PropTypes } from 'react';
+import { times } from 'lodash';
 import NavigationMenu from '../../NavigationMenu';
 import './main-layout.scss';
+
+const triangles = times(10, (i) => {
+    const style = { left: `${parseInt(Math.random() * 100 - 7, 10)}%` };
+
+    return (
+        <div className={`main-layout-bg-triangle main-layout-bg-triangle-${i}`} key={i} style={style} />
+    );
+});
 
 export default function MainLayout({ children }) {
     return (
         <div className="main-layout">
-            <div className="main-layout-bg" />
+            <div className="main-layout-bg">
+                {triangles}
+            </div>
             <NavigationMenu />
             <div className="main-content">
                 {children}
