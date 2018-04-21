f738311fa72a99add3a377b94d5274958a10b7d0
Russ Rinzler <russ.rinzler@gmail.com>
2018-04-21 15:00:11 -0700

    add Slack email images

--- src/components/pages/SlackPage/index.js
+++ src/components/pages/SlackPage/index.js
@@ -47,6 +47,61 @@ const imageSpecs = [
         sizes: [ '434' ],
         alt: 'onboarding-2-skip-step',
         extension: 'gif'
+    },
+    {
+        name: 'email-finish-creation',
+        sizes: [ '578' ],
+        alt: 'email-finish-creation'
+    },
+    {
+        name: 'email-invite',
+        sizes: [ '653' ],
+        alt: 'email-invite'
+    },
+    {
+        name: 'email-invite-reminder',
+        sizes: [ '677' ],
+        alt: 'email-invite-reminder'
+    },
+    {
+        name: 'email-no-account',
+        sizes: [ '596' ],
+        alt: 'email-no-account'
+    },
+    {
+        name: 'email-notifications',
+        sizes: [ '613' ],
+        alt: 'email-notifications'
+    },
+    {
+        name: 'email-password-logged-out',
+        sizes: [ '638' ],
+        alt: 'email-password-logged-out'
+    },
+    {
+        name: 'email-set-password',
+        sizes: [ '615' ],
+        alt: 'email-set-password'
+    },
+    {
+        name: 'email-team-creator-welcome',
+        sizes: [ '640' ],
+        alt: 'email-team-creator-welcome'
+    },
+    {
+        name: 'email-team-joiner-welcome',
+        sizes: [ '641' ],
+        alt: 'email-team-joiner-welcome'
+    },
+    {
+        name: 'email-whitelist-domain',
+        sizes: [ '674' ],
+        alt: 'email-whitelist-domain'
+    },
+    {
+        name: 'email-workplace-confirmation',
+        sizes: [ '599' ],
+        alt: 'email-workplace-confirmation'
     }
 ];
 
